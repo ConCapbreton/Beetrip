e5d31bfa442f91e2579bee7f39671b3b18921a8c
Connor <connorsexton@hotmail.com>
2025-02-06 13:54:36 +0100

    Added options to cors methods

--- Code/back/server.js
+++ Code/back/server.js
@@ -5,18 +5,18 @@ const matchRoutes = require('./routes/matchRoutes')
 
 const corsOptions = {
   origin: ['http://127.0.0.1:5500', 'https://letsplaytennis.netlify.app/'],
-  methods: ['POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true, 
+  methods: ['POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'], 
+  credentials: false, 
 }
 
 app.use(cors(corsOptions))
 
+app.options('*', cors(corsOptions))
+
 app.use(express.json())
   
 app.use('/letsplaytennis', matchRoutes)
-
-app.options('*', cors(corsOptions))
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
