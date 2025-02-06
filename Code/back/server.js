8563442a5760333319f2a811b5f6e6df788ec8b0
Connor <connorsexton@hotmail.com>
2025-02-06 12:36:05 +0100

    Update cors

--- Code/back/server.js
+++ Code/back/server.js
@@ -1,19 +1,22 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const matchRoutes = require('./routes/matchRoutes')
 
 app.use(express.json())
 
-app.use((_req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  res.setHeader("Content-Type", "application/json")
-  next()
-})
+
+app.use(cors())
+// app.use((_req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+//   res.setHeader("Content-Type", "application/json")
+//   next()
+// })
   
 app.use('/letsplaytennis', matchRoutes)
 
